94babdf8d9fdf31e16002d85cc7065f8be88f4ad
handysebastiann <handysebastzy@gmail.com>
2026-02-06 23:43:25 +0700

    styling px navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
             <!-- Page Heading -->
             @isset($header)
                 <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <div class="max-w-8xl mx-auto py-6 px-6 sm:px-8 lg:px-10">
                         {{ $header }}
                     </div>
                 </header>
